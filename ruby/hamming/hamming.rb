23b8ca3249cfa26eb2978569f24efb41bb3ef2cc
Blake Erickson <o.blakeerickson@gmail.com>
2014-04-14 09:09:25 -0600

    made code more readable by moving the location of the distance += 1

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,15 +1,11 @@
 class Hamming
-	class << self
-		def compute(strand1, strand2)
-			distance = 0
+	def self.compute(strand1, strand2)
+		distance = 0
 
-			[strand1.length, strand2.length].min.times do |i|
-				if strand1[i] != strand2[i]
-					distance += 1
-				end
-			end
-
-			distance
+		[strand1.length, strand2.length].min.times do |i|
+			distance += 1 if strand1[i] != strand2[i]
 		end
+
+		distance
 	end
 end
